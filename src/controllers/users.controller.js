94bb35f78b901f3aa53400bebb2ca9f1b65c5b6a
Parth Pandya <parth.pandya1307@gmail.com>
2026-02-23 22:45:56 +0530

    Bugs solved

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -164,7 +164,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
     await User.findByIdAndUpdate(
         req.user._id,
-        { $unset: { refreshToken: undefined } },
+        { $unset: { refreshToken: 1 } },
         { new: true }
     );
 
