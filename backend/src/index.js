706bd60f1f63db5d4737d9e14f953e4c7b0bd7be
Parth Pandya <parth.pandya1307@gmail.com>
2026-02-26 21:06:27 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import connectDB from "./db/index.js";
 import app from "./app.js";
 
-
-
-
+const PORT = process.env.PORT || 4000;
 
 connectDB()
-.then(()=>{app.listen(process.env.PORT , () => {
-    console.log(`Server is running on port ${process.env.PORT }`);
-})})
-.catch((error)=>{
-    console.log("Error connecting to MongoDB")
-    process.exit(1)
-})
+    .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+    })
+    .catch((error) => {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+    });
 
 /*
 ;(async ()=>{
